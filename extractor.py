1abcbe6ea673b854c02f34dba09562f05c8a4a66
lyfQAQ <0219lyf@gmail.com>
2025-01-23 18:40:23 +0800

    denormalize

--- extractor.py
+++ extractor.py
@@ -3,10 +3,16 @@ import numpy as np
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 class Extractor(object):
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K 
+        self.Kinv = np.linalg.inv(self.K)
+
+    def denormalize(self, pt):
+        return np.dot(self.Kinv, [pt[0], pt[1], 1.0])
+        # return int(round(pt[0] + self.w // 2)), int(round(pt[1] + self.h // 2))
 
     def extract(self, img):
         # detection 
@@ -27,14 +33,24 @@ class Extractor(object):
                     kp2 = self.last['kps'][m.trainIdx].pt
                     ret.append((kp1, kp2))
             
+        
+        # filter
         if len(ret) > 0:
             ret = np.array(ret)
+            print(ret.shape)
+
+            # normalize coords: subtract to move to 0
+            ret[:, :, 0] -= img.shape[0]//2
+            ret[:, :, 1] -= img.shape[1]//2
+
             model, inliers = ransac((ret[:,0], ret[:,1]),
                                     FundamentalMatrixTransform,
                                     min_samples=8, 
                                     residual_threshold=1,
                                     max_trials=100) 
             ret = ret[inliers]
+            s,v,d = np.linalg.svd(model.params)
+            print(v)
         self.last = {'kps': kps, 'des': des}
-
+       
         return ret
--- slam.py
+++ slam.py
@@ -8,9 +8,10 @@ W = int(1920//2)
 H = int(1080//2)
 
 dis = Display(W, H)
+F = 282
+K = np.array(([F,0,W//2],[0,F,H//2],[0, 0, 1]))
 
-
-fe = Extractor()
+fe = Extractor(K)
 
 def process_frame(img):
     img = cv2.resize(img, (dis.W, dis.H))
@@ -19,10 +20,11 @@ def process_frame(img):
         return
     print("%d matches" % len(matches))
 
+    
     for query_pt, train_pt in matches:
-        # print(query_pt)
-        x1,y1 = map(lambda x: int(round(x)), query_pt)
-        x2,y2 = map(lambda x: int(round(x)), train_pt)
+        x1,y1 = fe.denormalize(query_pt) 
+        x2,y2 = fe.denormalize(train_pt) 
+
         cv2.circle(img, (x1, y1), color=(0, 255, 0), radius=3)
         cv2.line(img, (x1, y1), (x2, y2), color=(255,0,0))
     dis.paint(img)
